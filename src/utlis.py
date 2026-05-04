0ba2e7a4906d4685d0b38e742f308d0756c97f2c
love kant sharma <sharmalovekant612@gmail.com>
2026-05-04 15:25:11 +0530

    model trainer

--- src/utlis.py
+++ src/utlis.py
@@ -20,7 +20,7 @@ def save_object(file_path, obj):
             pickle.dump(obj, file_obj)
 
     except Exception as e:
-        raise CustomException(e, sys)
+        raise customException(e, sys)
     
 def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
